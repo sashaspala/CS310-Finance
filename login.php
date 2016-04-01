c32f3a6305e9567f11efbdd061023540d944b49b
Jonathan Frey <freyjona@usc.edu>
2016-03-31 21:46:17 -0700

    login

--- login.php
+++ login.php
@@ -2,6 +2,10 @@
 	if(!session_id()) {
 		session_start();
 	}
+
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+
 	require_once("header.php");
 ?>	
 <head>
--- loginhandler.php
+++ loginhandler.php
@@ -1,17 +1,26 @@
 <?php
-	require_once("DataManager.php");
+	if(!session_id()) {
+		session_start();
+	}
+
+	// require_once("Classes/DataManager.php");
 
 	if (isset($_POST['login']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 		$user = DataManager::getInstance()->loginUser($_POST['email'], $_POST['password']);
 
 		if(is_null($user)) {
-			header('Location: http://www.reddit.com/');
+			header('Location: login.php');
 			exit();
 		}
 
 		$_SESSION['email'] = $_POST['email'];
 		$_SESSION['userID'] = $user->getUserID();
 
-		header('Location: http://www.google.com/');
+		header('Location: dashboard.php');
 		exit();
+
+	} else {
+		header('Location: login.php');
+		exit();
+	}
 ?>
