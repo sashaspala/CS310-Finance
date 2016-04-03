8af7a456dd8c80d8bf62c299b64d6fb8cabbb6e5
Chaitanya <pilaka@usc.edu>
2016-04-04 00:29:45 +0100

    to the end

--- Classes/DataManager.php
+++ Classes/DataManager.php
@@ -4,10 +4,10 @@ require_once("Classes/User.php");
 require_once("Classes/Account.php");
 require_once("Classes/Transaction.php");
 
-DataManager::getInstance()->addAccount('test',1);
-DataManager::getInstance()->addTransaction(date('Y-m-d'),99.99,"food","lots of stuff", "Ralphs",1,2);
+// DataManager::getInstance()->addAccount('test',1);
+// DataManager::getInstance()->addTransaction(date('Y-m-d'),99.99,"food","lots of stuff", "Ralphs",1,2);
 
-DataManager::getInstance()->addTransaction(date('Y-m-d'),99.99,"fdod","lots of stuff", "Ralphs",2,1);
+// DataManager::getInstance()->addTransaction(date('Y-m-d'),99.99,"fdod","lots of stuff", "Ralphs",2,1);
 
 
 
