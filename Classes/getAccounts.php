d6a8f09e9371f6a009f7151c7c9f33ff5add78c8
Ritvik <ritviksalim@gmail.com>
2016-04-14 20:06:21 -0700

    filter adn dashboard Tests

--- Classes/getAccounts.php
+++ Classes/getAccounts.php
@@ -1,7 +1,20 @@
 <?php
-
+require_once("Classes/DataManager.php");
 session_start();
+$accountsString= $_REQUEST["accounts"];
+$accountList = explode("-", $accountsString);
 
 
 
+for (var $accountIndex=0;$accountIndex<count($accountList);$accountIndex++ ){}
+	for (var $transactionIndex =0; $transactionIndex< count($transactlist) ; $transactionIndex++){
+		var $tempTransaction = DataManager::getInstance()->getAccountByName($accountList[$accountIndex]);//change account name made 
+		echo "<tr>";
+			echo "<td>" . $tempTransaction->getName() . "</td>";
+			echo "<td>" . $tempTransaction->getCategory() . "</td>";
+			echo "<td>" . $tempTransaction->getAmount() . "</td>";
+			echo "<td>" . $tempTransaction->getDate() . "</td>";
+		echo "</tr>";
+	}
+}
 ?>
