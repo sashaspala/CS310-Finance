137bd76b0482f7e72824529cd7c435f4f7e3a46a
Ritvik <ritviksalim@gmail.com>
2016-04-15 23:50:30 -0700

    testing datepicker

--- dashboard.php
+++ dashboard.php
@@ -101,6 +101,13 @@ $_SESSION['balanceSheet'] = $balanceSheet;
 			<div class="well" style="background-color:#FFFFFF">
 				<div id="graph" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 			</div>
+
+			<div class="input-group date" data-provide="datepicker">
+			    <input type="text" class="form-control">
+			    <div class="input-group-addon">
+			        <span class="glyphicon glyphicon-th"></span>
+			    </div>
+			</div>
 		</div>
 		</div>
 
