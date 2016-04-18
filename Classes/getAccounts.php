43a58f05435f899fb44e8dbeed1d27495c1ff9c5
DarvishKamalia <darvish.kamalia@gmail.com>
2016-04-17 18:46:08 -0700

    errors

--- Classes/getAccounts.php
+++ Classes/getAccounts.php
@@ -1,67 +1,65 @@
 <?php
+ require_once("DataManager.php");
 
-  include("DataManager.php");
-//
-// $accountsString = "TestAccountName7";
-// $accountList = explode("-", $accountsString);
-//
-// $accounts = array();
-// $transactions = array();
+$accountsString = "TestAccountName7";
+$accountList = explode("-", $accountsString);
+
+$accounts = array();
+$transactions = array();
 
 //get array of accounts
 
-// foreach ($accountList as $item) {
-// 	$account = DataManager::getInstance()->getAccount($item, 1);
-// 	array_push($accounts, $account);
-// 	# code...
-// }
-//
-// //get transactions
-// foreach ($accounts as $item) {
-// 	$accountTrans = DataManager::getInstance()->getTransactionsForAccount($item->getID(),1);
-// 	foreach($accountTrans as $accountItem) {
-// 		$transactions[] = $accountItem;
-// 	}
-// }
+foreach ($accountList as $item) {
+	$account = DataManager::getInstance()->getAccount($item, 1);
+	array_push($accounts, $account);
+	# code...
+}
 
-echo "<p>Hi</p>";
+//get transactions
+foreach ($accounts as $item) {
+	$accountTrans = DataManager::getInstance()->getTransactionsForAccount($item->getID(),1);
+	foreach($accountTrans as $accountItem) {
+		$transactions[] = $accountItem;
+	}
+}
 
-// //echo transactions
-// echo "<table id='transactions' class='table table-bordered table-hover sortable'>
-// 					<thead>
-// 						<tr>
-// 							<th>Name</th>
-// 							<th>Type</th>
-// 							<th>Amount</th>
-// 							<th>Date</th>
-// 						</tr>
-// 					</thead>
-// 					<tbody>";
-//
-// echo "<thead>";
-// echo"<tr>";
-// echo		"<th>Name</th>";
-// echo		"<th>Type</th>";
-// echo	"<th>Amount</th>";
-// echo "<th>Date</th>";
-// echo "</tr>";
-// echo "</thead>";
-// echo "<tbody>";
-//
-// foreach ($transactions as $item) {
-//
-// 	echo "<tr>";
-// 	echo "<td>" . $item->getName() . "</td>";
-//     echo "<td>" . $item->getCategory(). "</td>";
-//     echo "<td>" . $item->getAmount() . "</td>";
-//     echo "<td>" . $item->getDate() . "</td>";
-//     echo "<tr>";
-//
-// 	# code...
-// }
-//
-// echo "</tbody>";
-// echo "</table>";
+
+//echo transactions
+echo "<table id='transactions' class='table table-bordered table-hover sortable'>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Type</th>
+							<th>Amount</th>
+							<th>Date</th>
+						</tr>
+					</thead>
+					<tbody>";
+
+echo "<thead>";
+echo"<tr>";
+echo		"<th>Name</th>";
+echo		"<th>Type</th>";
+echo	"<th>Amount</th>";
+echo "<th>Date</th>";
+echo "</tr>";
+echo "</thead>";
+echo "<tbody>";
+
+foreach ($transactions as $item) {
+
+	echo "<tr>";
+	echo "<td>" . $item->getName() . "</td>";
+    echo "<td>" . $item->getCategory(). "</td>";
+    echo "<td>" . $item->getAmount() . "</td>";
+    echo "<td>" . $item->getDate() . "</td>";
+    echo "<tr>";
+
+	# code...
+}
+
+echo "</tbody>";
+echo "</table>";
 
 
 ?>
