fb3aaa055d3974a00ca2358a02efc04f29ded1f7
DarvishKamalia <darvish.kamalia@gmail.com>
2016-04-17 18:25:27 -0700

    errors

--- Classes/getAccounts.php
+++ Classes/getAccounts.php
@@ -59,20 +59,7 @@ foreach ($transactions as $item) {
 }
 
 echo "</tbody>";
-
-				echo "</table>";
-
+echo "</table>";
 
 
-// for (var $accountIndex=0;$accountIndex<count($accountList);$accountIndex++ ){}
-// 	for (var $transactionIndex =0; $transactionIndex< count($transactlist) ; $transactionIndex++){
-// 		var $tempTransaction = DataManager::getInstance()->getAccountByName($accountList[$accountIndex]);//change account name made
-// 		echo "<tr>";
-// 			echo "<td>" . $tempTransaction->getName() . "</td>";
-// 			echo "<td>" . $tempTransaction->getCategory() . "</td>";
-// 			echo "<td>" . $tempTransaction->getAmount() . "</td>";
-// 			echo "<td>" . $tempTransaction->getDate() . "</td>";
-// 		echo "</tr>";
-// 	}
-// }
 ?>
