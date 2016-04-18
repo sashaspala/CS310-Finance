9d902b2515000ab004a9e0f2a98aa57ad6036884
DarvishKamalia <darvish.kamalia@gmail.com>
2016-04-17 18:59:40 -0700

    errors

--- Classes/getAccounts.php
+++ Classes/getAccounts.php
@@ -2,57 +2,60 @@
  require_once("DataManager.php");
 
 $accountsString = $_GET["accounts"];
-$accountList = explode("-", $accountsString);
 
-$accounts = array();
-$transactions = array();
+echo ($accountsString);
 
-//get array of accounts
-
-foreach ($accountList as $item) {
-	$account = DataManager::getInstance()->getAccount($item, 1);
-	array_push($accounts, $account);
-	# code...
-}
-
-//get transactions
-foreach ($accounts as $item) {
-	$accountTrans = DataManager::getInstance()->getTransactionsForAccount($item->getID(),1);
-	foreach($accountTrans as $accountItem) {
-		$transactions[] = $accountItem;
-	}
-}
-
-
-//echo transactions
-echo "<table id='transactions' class='table table-bordered table-hover sortable'>";
-
-echo "<tbody>";
-
-echo "<thead>";
-echo "<tr>";
-echo		"<th>Name</th>";
-echo		"<th>Type</th>";
-echo	"<th>Amount</th>";
-echo "<th>Date</th>";
-echo "</tr>";
-echo "</thead>";
-echo "<tbody>";
-
-foreach ($transactions as $item) {
-
-	echo "<tr>";
-	echo "<td>" . $item->getName() . "</td>";
-    echo "<td>" . $item->getCategory(). "</td>";
-    echo "<td>" . $item->getAmount() . "</td>";
-    echo "<td>" . $item->getDate() . "</td>";
-    echo "<tr>";
-
-	# code...
-}
-
-echo "</tbody>";
-echo "</table>";
+// $accountList = explode("-", $accountsString);
+//
+// $accounts = array();
+// $transactions = array();
+//
+// //get array of accounts
+//
+// foreach ($accountList as $item) {
+// 	$account = DataManager::getInstance()->getAccount($item, 1);
+// 	array_push($accounts, $account);
+// 	# code...
+// }
+//
+// //get transactions
+// foreach ($accounts as $item) {
+// 	$accountTrans = DataManager::getInstance()->getTransactionsForAccount($item->getID(),1);
+// 	foreach($accountTrans as $accountItem) {
+// 		$transactions[] = $accountItem;
+// 	}
+// }
+//
+//
+// //echo transactions
+// echo "<table id='transactions' class='table table-bordered table-hover sortable'>";
+//
+// echo "<tbody>";
+//
+// echo "<thead>";
+// echo "<tr>";
+// echo		"<th>Name</th>";
+// echo		"<th>Type</th>";
+// echo	"<th>Amount</th>";
+// echo "<th>Date</th>";
+// echo "</tr>";
+// echo "</thead>";
+// echo "<tbody>";
+//
+// foreach ($transactions as $item) {
+//
+// 	echo "<tr>";
+// 	echo "<td>" . $item->getName() . "</td>";
+//     echo "<td>" . $item->getCategory(). "</td>";
+//     echo "<td>" . $item->getAmount() . "</td>";
+//     echo "<td>" . $item->getDate() . "</td>";
+//     echo "<tr>";
+//
+// 	# code...
+// }
+//
+// echo "</tbody>";
+// echo "</table>";
 
 
 ?>
