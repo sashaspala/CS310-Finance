b10aa66f936f5d089bbe6376f6b6412d01586fc2
Alex Hong <hongalex@usc.edu>
2016-04-25 16:17:02 -0700

    checking using addUsers

--- addUsers.sql
+++ addUsers.sql
@@ -1,4 +1,4 @@
 insert into Users values 
-	(0, 'Darvish', 'Kamalia', 'swag@swag.com', 'swag'),
-	(null, 'Alex', 'Hong', 'moreswag@moreswag.com', 'moreswag'),
-	(null, 'Test', 'Test', 'test@test.com', 'test');
+	(1, 'Darvish', 'Kamalia', 'swag@swag.com', 'swag'),
+	(2, 'Alex', 'Hong', 'moreswag@moreswag.com', 'moreswag'),
+	(3, 'Test', 'Test', 'test@test.com', 'test');
