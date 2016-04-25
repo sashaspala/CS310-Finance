17c9460e7eb89745e684cabfeb6699e217761768
Alex Hong <hongalex@usc.edu>
2016-04-25 16:49:39 -0700

    csvhandler test

--- csvhandler.php
+++ csvhandler.php
@@ -7,33 +7,35 @@
 	$line=0;
 	$numOfAccount=$data[$line][0];
 	$line++;
-	for($i = 0; $i <$numOfAccount;$i++){
-		
+	
+	for($i = 0; $i <$numOfAccount; $i++){
 		$accountName=$data[$line][0];
 		$numOfTransactions=$data[$line][1];
 		$line++;
 	 	// CREATES AN ACCOUNT IN DB
 		// DataManager::getInstance()->addTransaction(date('Y-m-d'),99.99,"food","lots gczvxcvzxcvzx of stuff", "Ralphxzcvxcvzxcs",1,2);
 		$account=DataManager::getInstance()->addAccount($accountName,1);
-		if($i==0){echo $numOfAccount;}
-		for($j=0;$j<$numOfTransactions;$j++){
-				$transactionName=$data[$line][0];
-				$transactionPrincipal=$data[$line][1];
-				$transactionDate=$data[$line][2];
-				$transactionAmount=$data[$line][3];
-				$transactionCategory=$data[$line][4];
-				//CREATES A TRANSACTIO IN DB
-				$transaction=DataManager::getInstance()->addTransaction($transactionDate, $transactionAmount, $transactionCategory, $transactionName, $transactionPrincipal,  $account->getID(), 1);
-				if($transactionAmount<0){
-					//add it to Liabilities Account;
-				}else{
-					//add it to Assets Account
-				}
-				$line++;
+		if($i == 0) echo $numOfAccount;
+
+		for($j = 0; $j < $numOfTransactions; $j++){
+			echo $numOfTransactions;
+			$transactionName=$data[$line][0];
+			$transactionPrincipal=$data[$line][1];
+			$transactionDate=$data[$line][2];
+			$transactionAmount=$data[$line][3];
+			$transactionCategory=$data[$line][4];
+			//CREATES A TRANSACTION IN DB
+			$transaction=DataManager::getInstance()->addTransaction($transactionDate, $transactionAmount, $transactionCategory, $transactionName, $transactionPrincipal,  $account->getID(), 1);
+			if($transactionAmount<0){
+				//add it to Liabilities Account;
+			}else{
+				//add it to Assets Account
+			}
+			$line++;
 		}
 
-		$line++;
-	// 	// ADD ACCOUNT TO BALLANCESHEET LOCALLY
+		//$line++;
+	 	// ADD ACCOUNT TO BALANCESHEET LOCALLY
 		
 		 $_SESSION['balanceSheet']->addAccount($account);
 	}
