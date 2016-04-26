8e405ba598f5ba0aede62261dcd943543486afd2
Jonathan Frey <freyjona@usc.edu>
2016-04-26 14:24:09 -0700

    budget?

--- dashboard.php
+++ dashboard.php
@@ -262,12 +262,12 @@ $_SESSION['dataManager'] = DataManager::getInstance();
 								$budget = $_POST['budgets'];
 
 								 switch ($budget) {
-								 	case "Food":
-								 		echo "<h2>Total budget: $300.00</h2>"
-								 		break;
-								 	case "Education":
-								 		echo "<h2>Total budget: $500.00</h2>"
-								 		break;
+								 	// case "Food":
+								 	// 	echo "<h2>Total budget: $300.00</h2>"
+								 	// 	break;
+								 	// case "Education":
+								 	// 	echo "<h2>Total budget: $500.00</h2>"
+								 	// 	break;
 								 	default:
 								 		echo "<p>ERROR!</p>";
 								 		break;
