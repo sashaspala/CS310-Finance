aee72fa31860beaca9d6ded0dfedc357fba1a4bc
Jonathan Frey <freyjona@usc.edu>
2016-04-26 14:37:39 -0700

    budget?

--- selectBudget.php
+++ selectBudget.php
@@ -10,9 +10,9 @@
 
 		echo $budget;
 	 	switch ($budget) {
-	 	// 	case "Food":
-	 	// 		echo "<h2>Total budget: $300.00</h2>"
-	 	// 		break;
+	 		case "Food":
+	 			echo "<h2>Total budget: $300.00</h2>"
+	 			break;
 	 	// 	case "Education":
 	 	// 		echo "<h2>Total budget: $500.00</h2>"
 	 	// 		break;
