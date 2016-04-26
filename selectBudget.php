987e9968e6880c93f2985d021642318f1cdd9456
Jonathan Frey <freyjona@usc.edu>
2016-04-26 12:59:39 -0700

    small fix

--- selectBudget.php
+++ selectBudget.php
@@ -9,9 +9,9 @@
 		$budget = $_POST['budgets'];
 
 		 switch ($budget) {
-		 	case 'Food':
+		 	case "Food":
 		 		echo "<h2>Total budget: $300.00</h2>"
-		 	case 'Education':
+		 	case "Education":
 		 		echo "<h2>Total budget: $500.00</h2>"
 		 	default:
 		 		echo "<p>ERROR!</p>";
