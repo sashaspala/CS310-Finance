db2868a2e81bccbfcf21627307a48fb0668e58db
Jonathan Frey <freyjona@usc.edu>
2016-04-26 14:36:37 -0700

    budget?

--- selectBudget.php
+++ selectBudget.php
@@ -9,16 +9,16 @@
 		$budget = $_POST['budgets'];
 
 		echo $budget;
-		 // switch ($budget) {
-		 // 	case "Food":
-		 // 		echo "<h2>Total budget: $300.00</h2>"
-		 // 		break;
-		 // 	case "Education":
-		 // 		echo "<h2>Total budget: $500.00</h2>"
-		 // 		break;
-		 // 	default:
-		 // 		echo "<p>ERROR!</p>";
-		 // 		break;
-		 // }
+	 	switch ($budget) {
+	 	// 	case "Food":
+	 	// 		echo "<h2>Total budget: $300.00</h2>"
+	 	// 		break;
+	 	// 	case "Education":
+	 	// 		echo "<h2>Total budget: $500.00</h2>"
+	 	// 		break;
+	 		default:
+	 			echo "<p>ERROR!</p>";
+	 			break;
+	 	}
 	}
 ?>
