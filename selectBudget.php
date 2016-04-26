7af5ec7009bbb1d10c234407b7cbd3eb8172379f
Jonathan Frey <freyjona@usc.edu>
2016-04-26 12:56:13 -0700

    small fix

--- selectBudget.php
+++ selectBudget.php
@@ -5,16 +5,16 @@
 	
 	require_once("Classes/DataManager.php");
 
-	if(isset($_POST['budgets'])){
-		$budget = $_POST['budgets'];
+	// if(isset($_POST['budgets'])){
+	// 	$budget = $_POST['budgets'];
 
-		 switch ($select1) {
-		 	case 'Food':
-		 		echo "<h2>Total budget: $300.00</h2>"
-		 	case 'Education':
-		 		echo "<h2>Total budget: $500.00</h2>"
-		 	default:
-		 		echo "<p>ERROR!</p>";
-		 }
-	}
+	// 	 switch ($select1) {
+	// 	 	case 'Food':
+	// 	 		echo "<h2>Total budget: $300.00</h2>"
+	// 	 	case 'Education':
+	// 	 		echo "<h2>Total budget: $500.00</h2>"
+	// 	 	default:
+	// 	 		echo "<p>ERROR!</p>";
+	// 	 }
+	// }
 ?>
