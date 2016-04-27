c713348ee7d5e43268be86a097f856fa2b3351d3
emilyl <zhifeili@usc.edu>
2016-04-27 00:42:22 -0700

    sdfS

--- timeout.php
+++ timeout.php
@@ -7,17 +7,18 @@
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 
-	echo "<div style ="
-		display:none;
-		top:0px;
-		left:0px;
-		position:absolute;
-		z-index:100; 
-		height: 100%; 
-		width:100%; 
-		background-color:rgba(100,80,80,0.8);
-		color:white; padding-top:20%; text-align:center;
-		">
+	echo "<div 
+		// style ="
+		// display:none;
+		// top:0px;
+		// left:0px;
+		// position:absolute;
+		// z-index:100; 
+		// height: 100%; 
+		// width:100%; 
+		// background-color:rgba(100,80,80,0.8);
+		// color:white; padding-top:20%; text-align:center;"
+		>
 		<h1>dsfsdf</h2>
 		</div>
 
