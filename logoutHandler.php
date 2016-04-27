281d3a428509d284981b3497151baf39b16fb635
emilyl <zhifeili@usc.edu>
2016-04-26 22:48:03 -0700

    asdgf

--- logoutHandler.php
+++ logoutHandler.php
@@ -6,10 +6,12 @@
 	require_once("Classes/DataManager.php");
 
 	// unset($_SESSION);
-	unset($_SESSION['userID']);
-	//unset($_SESSION['userFullName']);
-	unset($_SESSION['timeout'])
-	//unset($_SESSION['b'])
+	echo isset($_SESSION['userID']);
+	echo isset($_SESSION['timeout']);
+	// unset($_SESSION['userID']);
+	// //unset($_SESSION['userFullName']);
+	// unset($_SESSION['timeout'])
+	// //unset($_SESSION['b'])
 
 	DataManager::getInstance()->logout();
 	header('Location: login.php');
