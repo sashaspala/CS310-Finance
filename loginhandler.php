1c3d6c706168171c09f987b79e3fdba4010bb50e
emilyl <zhifeili@usc.edu>
2016-04-26 23:29:47 -0700

    Adasd

--- loginhandler.php
+++ loginhandler.php
@@ -5,9 +5,6 @@
 
 	require_once("Classes/DataManager.php");
 
-
-
-
 	if (isset($_POST['login']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
 		$user = DataManager::getInstance()->loginUser($_POST['email'], md5($_POST['password']));
@@ -15,12 +12,14 @@
 		if(is_null($user)) {
 			echo '<p class="bg-danger">'.'done'.'</p>';
 			header('Location: login.php');
+			$_SESSION['timeout_count'] += 1;
 		//	exit();
 		}
 		else if(is_null($user->getUserID())) {
 			echo '<p class="bg-danger">'.'done'.'</p>';
 			header('Location: login.php');
 			$failed_attempts += 1;
+			$_SESSION['timeout_count'] += 1;
 		//	exit();
 		}
 
@@ -29,6 +28,8 @@
 		$_SESSION['userFullName']= $user->getFirstName() ." ". $user->getLastName();
 		$_SESSION["blah"]= "goesfromlogin";
 		header('Location: dashboard.php');
+		$_SESSION['timeout_count'] = 0;
+		$_SESSION['timeout'] = 0;
 	//	exit();
 
 	} else {
