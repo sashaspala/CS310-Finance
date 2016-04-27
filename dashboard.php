a795ccaa59f6c8afeb737a7f6c4e28d7af23aaba
Ritvik <ritviksalim@gmail.com>
2016-04-27 16:13:37 -0700

    dateChangeHandler

--- dashboard.php
+++ dashboard.php
@@ -344,19 +344,7 @@ $accounts = DataManager::getInstance()->getAccountsForUser(1);
             verticalAlign: 'middle',
             borderWidth: 0
         },
-        series: [{
-            name: 'Liabilities',
-            data: [7.0, 6.9, 9.5]
-        }, {
-            name: 'Assets',
-            data: [-0.2, 0.8, 5.7]
-        }, {
-            name: 'Net Worth',
-            data: [-0.9, 0.6, 3.5]
-        }, {
-            name: 'Account 1', //this should update automatically from some function call
-            data: [3.9, 4.2, 5.7]
-        }]
+        series: [{"name":"HSBC","data":[33,54,54]}]
     });
 });
 
