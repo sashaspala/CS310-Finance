aef9e5b43c14f75eaa7c1c58f86bf93479da1047
emilyl <zhifeili@usc.edu>
2016-04-26 23:40:23 -0700

    sdfsdf

--- login.php
+++ login.php
@@ -7,6 +7,8 @@
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 
+	
+
 	if (!isset($_SESSION['timeout']))
 	{
 		echo "setting timerout";
--- timeout.php
+++ timeout.php
@@ -1,13 +1,13 @@
 <?php
-	// if(!session_id()) {
+	if(!session_id()) {
 		session_start();
-	// }
+	}
 	require_once("header.php");
 
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 
-	while ($_SESSION['timeout'] + 20 > time())
+	while ($_SESSION['timeout'] + 5 > time())
 	{
 		echo "";
 	}
