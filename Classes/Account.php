67fc20679405b66abda1ddfdacabe0d0047d586e
Ritvik <ritviksalim@gmail.com>
2016-04-27 13:22:02 -0700

    dateChangeHandler

--- Classes/Account.php
+++ Classes/Account.php
@@ -81,6 +81,16 @@ class Account {
 	public function calculateDataPoint($startDate, $endDate, $numOfPoints){
 		$startIndex=0;
 		$this->transactions = DataManager::getInstance()->getTransactionsForAccount($this->accountID, 1);
+		if(count($this->transactions)>0){
+			array_push($this->netValues, $this->transactions[0]->getAmount());
+			// for ($index =1; $index<count($this->transactions); $index++){
+			// 	//I THINK THIS LINE DOES NOT WORK CURRENTLY BECAUSE OF THE SAMPLE DATA
+			// 	$prev_Sum= $this->netValues[$index-1]+$this->transactions[$index]->getAmount();
+			// 	echo "string "+$prev_Sum+"<br>";
+			// 	array_push($this->netValues, $prev_Sum;
+			// }
+		}
+
 		//echo $this->accountID;
 		//var_dump($this->transactions); 
 		//var_dump($this);
