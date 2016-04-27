2f3bebd1adaa3403186163bb012ad3be5aba81c8
Jonathan Frey <freyjona@usc.edu>
2016-04-26 23:54:39 -0700

    kms

--- selectBudget.php
+++ selectBudget.php
@@ -32,21 +32,6 @@
     	'December' => '500',
 	);
 
-	$months_to_transactions = array(
-    	'January' => '01/%',
-    	'February' => '02/%',
-    	'March' => '03/%',
-    	'April' => '04/%',
-    	'May' => '05/%',
-    	'June' => '06/%',
-    	'July' => '07/%',
-    	'August' => '08/%',
-    	'September' => '09/%',
-    	'October' => '10/%',
-    	'November' => '11/%',
-    	'December' => '12/%',
-	);
-
 	if(isset($_POST['budgets']) && isset($_POST['months'])){
 		$budget = $_POST['budgets'];
 		$month = $_POST['months'];
@@ -75,7 +60,7 @@
 		 			<h3>Food</h3>
 		 			<h4>Total budget: $<?php echo $food[$month] ?>.00</h4>
 		 			<?php
-		 			// $transactions = DataManager::getInstance()->getTransactionsForMonth($months_to_transactions[$month], 1);
+		 				$transactions = DataManager::getInstance()->getTransactionsForMonth($month);
 		 			// $total = 0;
 		 			// foreach($transactions as $transaction) {
 		 			// 	$total += $transaction->getAmount();
