fb6610edb8334fa2515a0de41c039dab5a2fd6ba
Jonathan Frey <freyjona@usc.edu>
2016-04-26 20:51:00 -0700

    kms

--- selectBudget.php
+++ selectBudget.php
@@ -2,8 +2,35 @@
 
 	require_once("Classes/DataManager.php");
 
-	define("FOOD", "300.00");
-	define("EDUCATION", "500.00");
+	$food = array(
+    	'January' => '300',
+    	'February' => '300',
+    	'March' => '300',
+    	'April' => '300',
+    	'May' => '300',
+    	'June' => '300',
+    	'July' => '300',
+    	'August' => '300',
+    	'September' => '300',
+    	'October' => '300',
+    	'November' => '300',
+    	'December' => '300',
+	);
+
+	$education = array(
+    	'January' => '500',
+    	'February' => '500',
+    	'March' => '500',
+    	'April' => '500',
+    	'May' => '500',
+    	'June' => '500',
+    	'July' => '500',
+    	'August' => '500',
+    	'September' => '500',
+    	'October' => '500',
+    	'November' => '500',
+    	'December' => '500',
+	);
 
 	if(isset($_POST['budgets']) && isset($_POST['months'])){
 		$budget = $_POST['budgets'];
@@ -11,7 +38,7 @@
 
 		if($budget != 'Select a budget' && $month != 'Select a month') {
 			?>
-			<form id="setBudget" class="form-set-budget" action='setBudget.php' method='post' accept-charset='UTF-8'>
+			<form id="setBudget" class="form-set-budget" method='post' accept-charset='UTF-8'>
 			<font size="3">Set Budget Amount: $</font>
 			<input type="number" name="budgetAmount" min="0" max="9999" style="width:50px;margin-right:10px">
 			<input type="submit" id="bugdetSubmit" name="bugdetSubmit" value= "Set Budget" class="btn btn-info">
@@ -25,7 +52,7 @@
 		 			break;
 		 		case 'Food': ?>
 		 			<h3>Food</h3>
-		 			<h4>Total budget: $300.00</h4>
+		 			<h4>Total budget: <?php echo $food[$month] ?></h4>
 		 			<?php
 		 			break;
 		 		case 'Education': ?>
