6fbcae0c665a4cbf3957c4483ad94124262d9948
Ritvik <ritviksalim@gmail.com>
2016-04-27 12:19:37 -0700

    dateChangeHandler

--- Classes/Account.php
+++ Classes/Account.php
@@ -107,12 +107,12 @@ class Account {
 		unset($index); 
 		echo 'INDICES'; 
 		echo $startIndex; 
-		echo $endIndex; 
+		echo $endIndex."<br>"; 
 		$netValuesIndex=$startIndex;
 		$currDate=$startDate;
 		for($index=0; $index<$numOfPoints;$index++){//because we need that many points
 			echo "current date is";
-			echo $currDate;
+			echo $currDate."<br>";
 			if($currDate>strtotime($this->transactions[$netValuesIndex]->getDate())){
 				$netValuesIndex++;
 			}
