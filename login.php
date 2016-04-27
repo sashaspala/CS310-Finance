8079f3d838c025350fcf7cb89686b207cab1a013
emilyl <zhifeili@usc.edu>
2016-04-26 22:37:02 -0700

    asdsad

--- login.php
+++ login.php
@@ -7,8 +7,8 @@
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 
-	unset($_SESSION['timeout']);
-	unset($_SESSION['userID']);
+	// unset($_SESSION['timeout']);
+	// unset($_SESSION['userID']);
 
 	echo "asdasdasdasdasdas";
 	if (!isset($_SESSION['timeout']))
