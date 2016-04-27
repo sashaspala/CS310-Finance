48b6b63083bbfc1f03132492dadc17d2b0daca1b
Ritvik <ritviksalim@gmail.com>
2016-04-27 13:20:23 -0700

    dateChangeHandler

--- Classes/Account.php
+++ Classes/Account.php
@@ -75,17 +75,17 @@ class Account {
 		//var_dump($this->transactions); 
 		//var_dump($this);
 
-		if(count($this->transactions)>0){
+		// if(count($this->transactions)>0){
 
-			//TODO need to fix the below commented code
-			array_push($this->netValues, $this->transactions[0]->getAmount());
-			for ($index =1; $index<count($this->transactions); $index++){
-				//I THINK THIS LINE DOES NOT WORK CURRENTLY BECAUSE OF THE SAMPLE DATA
-				$prev_Sum= $this->netValues[$index-1]+$this->transactions[$index]->getAmount();
-				echo "string "+$prev_Sum+"<br>";
-				array_push($this->netValues, $prev_Sum;
-			}
-		}
+		// 	//TODO need to fix the below commented code
+		// 	array_push($this->netValues, $this->transactions[0]->getAmount());
+		// 	for ($index =1; $index<count($this->transactions); $index++){
+		// 		//I THINK THIS LINE DOES NOT WORK CURRENTLY BECAUSE OF THE SAMPLE DATA
+		// 		$prev_Sum= $this->netValues[$index-1]+$this->transactions[$index]->getAmount();
+		// 		echo "string "+$prev_Sum+"<br>";
+		// 		array_push($this->netValues, $prev_Sum;
+		// 	}
+		// }
 
 		echo "it got here <br>"
 		$endIndex=count($this->transactions)-1;
