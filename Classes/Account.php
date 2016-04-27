11f8ecf0b4a91056e201834098387a31b8c73277
Ritvik <ritviksalim@gmail.com>
2016-04-27 13:30:22 -0700

    dateChangeHandler

--- Classes/Account.php
+++ Classes/Account.php
@@ -87,7 +87,7 @@ class Account {
 			for ($index =1; $index<count($this->transactions); $index++){
 			// 	//I THINK THIS LINE DOES NOT WORK CURRENTLY BECAUSE OF THE SAMPLE DATA
 				$prev_Sum= $this->netValues[$index-1]+$this->transactions[$index]->getAmount();
-				echo "string "+$prev_Sum+"<br>";
+				echo "string ".$prev_Sum."<br>";
 				array_push($this->netValues, $prev_Sum);
 			}
 		}
