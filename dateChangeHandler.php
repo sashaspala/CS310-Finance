0ec359642c3018112eb24a9c75186ccbe498609c
Ritvik <ritviksalim@gmail.com>
2016-04-27 17:39:47 -0700

    dateChangeHandler FIXING ASSETS

--- dateChangeHandler.php
+++ dateChangeHandler.php
@@ -31,15 +31,15 @@
 		$accountNEG->setTransactions(DataManager::getInstance()->getNegativeTransactionsForUser());
 		array_push($accountList, $accountALL);
 		array_push($accountList, $accountPOS);
-		// array_push($accountList, $accountNEG);
+		array_push($accountList, $accountNEG);
 		//echo "121212HELPPPPOPPOPPOPO121212";
-		for ($index=0; $index<count($accountList) - 2; $index++){
+		for ($index=0; $index<count($accountList) - 3; $index++){
 
 			//echo " hi".$index."\n";
 			$accountList[$index]->calculateDataPoint($start, $end, $days,true);
 			//echo "god";
 		}
-		for ($index=count($accountList)-2;$index<count($accountList);$index++){
+		for ($index=count($accountList)-3;$index<count($accountList);$index++){
 			$accountList[$index]->calculateDataPoint($start, $end, $days,false);
 		}
 		// header('Location: 2323.php');
